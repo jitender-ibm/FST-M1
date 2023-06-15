3d3ce9ae92cac80335a3000cc31f6eb4834ffe01
jitender-ibm <132567780+jitender-ibm@users.noreply.github.com>
2023-06-15 21:12:24 +0530

    Added files via upload

---
+++ API/Project/FST_RestAssuredProject/TestClass.java
@@ -0,0 +1,71 @@
+package testPackage;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestClass {
+
+    RequestSpecification requestSpec;
+    String sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII1GbZizlWbNSFtDhE0+QRsKLLzbcyq2xJimUfWBWO0R";
+    int id;
+
+    @BeforeClass
+    public void setUp() {
+        // Create request specification
+        requestSpec = new RequestSpecBuilder()
+                // Set content type
+                .setContentType(ContentType.JSON)
+                // Set base URL
+                .setBaseUri("https://api.github.com")
+                // Build request specification
+                .build();
+
+        /*responseSpec = new ResponseSpecBuilder()
+                // Check status code in response
+                .expectStatusCode(200)
+                // Check response content type
+                .expectContentType("application/json")
+                // Check if response contains name property
+                .expectBody("status", equalTo("alive"))
+                // Build response specification
+                .build();*/
+    }
+
+    @Test()
+    public void postRequest(int id, String name, String status) {
+        Response response = given().spec(requestSpec) // Use requestSpec
+                .pathParam("petId", id) // Add path parameter
+                .when().delete("/{petId}"); // Send GET request
+
+        // Assertions
+        response.then().body("code", equalTo(200));
+    }
+
+    @Test()
+    public void getRequest(int id, String name, String status) {
+        Response response = given().spec(requestSpec) // Use requestSpec
+                .pathParam("petId", id) // Add path parameter
+                .when().delete("/{petId}"); // Send GET request
+
+        // Assertions
+        response.then().body("code", equalTo(200));
+    }
+
+    @Test()
+    public void deleteRequest(int id, String name, String status) {
+        Response response = given().spec(requestSpec) // Use requestSpec
+                .pathParam("petId", id) // Add path parameter
+                .when().delete("/{petId}"); // Send GET request
+
+        // Assertions
+        response.then().body("code", equalTo(200));
+    }
+}
